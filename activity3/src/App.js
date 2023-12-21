fb67c300cd9dcf9bbbaaaecabec7852429e821c3
JudeBanar <judemichael.banar@tup.edu.ph>
2023-12-21 23:39:17 +0800

    Added the reset number button

--- activity3/src/App.js
+++ activity3/src/App.js
@@ -3,13 +3,18 @@ import { useState } from "react";
 
 function App() {
 
-  const [numbr, setNumbr] = useState(1);
+  const [numbr, setNumbr] = useState(0);
+
   const increaseNumbr = () => {
     setNumbr(numbr + 1);
   }
 
   const decreaseNumbr = () => {
     setNumbr(numbr - 1);
+  }
+
+  const resetNumbr = () => {
+    setNumbr(0)
   }
 
   return (
@@ -20,6 +25,7 @@ function App() {
       <div className='Btn'>
         <button onClick={increaseNumbr}> Increase Number </button>
         <button onClick={decreaseNumbr}> Decrease Number </button>
+        <button onClick={resetNumbr}> Reset Number </button>
       </div>
     </div>
   );
