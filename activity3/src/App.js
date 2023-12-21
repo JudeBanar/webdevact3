eda386c1d0c4df40e1e8c2b15027b416b4950985
JudeBanar <judemichael.banar@tup.edu.ph>
2023-12-21 23:36:22 +0800

    Added the number display and the increase number button

--- activity3/src/App.js
+++ activity3/src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [numbr, setNumbr] = useState(1);
+  const increaseNumbr = () => {
+    setNumbr(numbr + 1);
+  }
+
   return (
-    
+    <div className='App'>
+      <div className='Nmbr'>
+        <h2> {numbr} </h2>
+      </div>
+      <div className='Btn'>
+        <button onClick={increaseNumbr}> Increase Number </button>
+      </div>
+    </div>
   );
 }
 
